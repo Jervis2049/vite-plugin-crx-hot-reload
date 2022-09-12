43ce8053f5ed2a2a7cc0fc60e07566ac3a148736
Jervis <1044131284@qq.com>
2022-09-12 15:50:25 +0800

    chore: eslint allowEmptyCatch

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
       // we are only using this rule to check for unused arguments since TS
       // catches unused variables but not args.
       { varsIgnorePattern: '.*', args: 'none' }
-    ]
+    ],
+    'no-empty': ['error', { allowEmptyCatch: true }]
   }
 }
